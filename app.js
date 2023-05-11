bb078414a8d0d4fe1a4f652a8e6b2738a29572d4
Julius Ojakaa <ojayallday@gmail.com>
2023-05-11 04:58:22 +0300

    delani image file , java script files, app, js configuration, home .ejs configuration

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const homeRouter = require ("./routes/home");
 
 const app = express();
+//det view engine to ejs
+app.set('view engine', "ejs")
+// set middleware
+app.use(homeRouter);
 
 app.listen(5500, ()=>{
     console.log("Application is starting...")
