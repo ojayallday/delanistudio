547da32a7f3951dbe99174bb3b8fbad3a0fcca32
Deus Nyakundi <deusnyakundi@Alchemist-2.local>
2023-06-22 12:44:01 +0300

    Login dependencies

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const homeRouter = require ("./routes/home.js");
 
 const app = express();
 
-
+/*
 app.get('/', (req, res) => {
     res.send('<a href="/auth/goggle"> Login with Google <a>');
 })
@@ -11,12 +11,17 @@ app.get('/', (req, res) => {
 app.get('/protected', (req,res) => {
     res.send('Hello!');
 })
+*/
 
 //set view engine to ejs
 app.set('view engine', "ejs")
 
 // set middleware
 app.use(homeRouter.router);
+
+//Telling express that all our assets are available here
+app.use(express.static(__dirname + '/public'));
+
 
 app.listen(5500, ()=>{
     console.log("Application is starting...")
